d0be26d6a168af4701d3eec42f8850a99a4dfee1
Szabo Vilmos <vili8899s@gmail.com>
2019-02-20 22:13:09 +0100

    done

---
+++ Hash.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Project\Helpers;
+
+class Hash
+{
+    public static function create($password)
+    {
+        return password_hash($password, PASSWORD_BCRYPT, ['cost' => 10]);
+    }
+
+    public static function check($password, $hash)
+    {
+        return password_verify($password,$hash);
+    }
+
+}
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+use Project\Helpers\Hash;
+require 'Hash.php';
+
+echo var_dump(Hash::check('cats','$2y$10$ODptYivcSAl6OmV9u97.kuP/cCBV5424LXjudGVaNoEEeWvfYfTEy'));
